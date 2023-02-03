879c49c56e1ad9bce507cb91cc61f34b6a423888
Artur Sgibnev <55087938+Artur-Sg@users.noreply.github.com>
2023-02-04 03:45:41 +0400

    Customizable styles, theme provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,15 @@ import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async';
 import { Link, Route, Routes } from 'react-router-dom';
 import { MainPageAsync } from '../pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
-import '../index.scss';
+import { useTheme } from '../theme/useTheme';
+import '../styles/index.scss';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Change style</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About Page</Link>
       <Suspense fallback={<div>Loading...</div>}>
