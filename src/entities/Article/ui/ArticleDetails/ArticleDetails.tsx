c6702fc477796388e461c8d83699dcb7cf7d62ff
Artur Sgibnev <55087938+Artur-Sg@users.noreply.github.com>
2023-09-09 01:05:47 +0400

    feat(article): Add article details, icon wrapper

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -2,35 +2,37 @@ import { useTranslation } from 'react-i18next';
 import { memo, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import { classNames } from '@shared/lib/classNames/classNames';
+import EyeIcon from '@shared/assets/icons/eye.svg';
+import CalendarIcon from '@shared/assets/icons/calendar.svg';
 import DynamicModuleLoader, { ReducersList } from '@shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import AppText, { AppTextAlign } from '@shared/ui/AppText/AppText';
+import AppText, { AppTextAlign, AppTextSize } from '@shared/ui/AppText/AppText';
 import { AppSkeleton } from '@shared/ui/AppSkeleton/AppSkeleton';
 import { useAppDispatch } from '@shared/lib/hooks/useAppDispatch';
 
 import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
 import { fetchArticleById } from '../../model/services';
 import {
-  // getArticleDetailsData,
+  getArticleDetailsData,
   getArticleDetailsError,
   getArticleDetailsIsLoading,
 } from '../../model/selectors/articleDetails';
 import cls from './ArticleDetails.module.scss';
+import { AppAvatar } from '../../../../shared/ui/AppAvatar/AppAvatar';
+import { AppIcon } from '../../../../shared/ui/AppIcon/AppIcon';
 
 interface ArticleDetailsProps {
   id: string;
-  className?: string;
 }
 
 export const ArticleDetails = memo((props: ArticleDetailsProps) => {
-  const { className, id } = props;
+  const { id } = props;
   const { t } = useTranslation('article');
   const reducers: ReducersList = {
     articleDetails: articleDetailsReducer,
   };
   const dispatch = useAppDispatch();
   const isLoading = useSelector(getArticleDetailsIsLoading);
-  // const article = useSelector(getArticleDetailsData);
+  const article = useSelector(getArticleDetailsData);
   const error = useSelector(getArticleDetailsError);
 
   useEffect(() => {
@@ -41,18 +43,36 @@ export const ArticleDetails = memo((props: ArticleDetailsProps) => {
 
   if (isLoading) {
     content = (
-      <div>
+      <>
         <AppSkeleton className={cls.avatar} width={200} height={200} borderRadius="50%" />
         <AppSkeleton className={cls.title} width={300} height={32} />
         <AppSkeleton className={cls.skeleton} width={600} height={24} />
         <AppSkeleton className={cls.skeleton} width="100%" height={200} />
         <AppSkeleton className={cls.skeleton} width="100%" height={200} />
-      </div>
+      </>
     );
   } else if (error) {
     content = <AppText align={AppTextAlign.CENTER} text={error} title={t('Error')} />;
   } else {
-    content = <div className={classNames(cls.articleDetails, [className], {})}>{t('articleDetailsPage')}</div>;
+    content = (
+      <>
+        <div className={cls.avatarWrapper}>
+          <AppAvatar size={200} src={article?.img} className={cls.avatar} />
+        </div>
+
+        <AppText className={cls.title} title={article?.title} text={article?.subtitle} size={AppTextSize.L} />
+
+        <div className={cls.articleInfo}>
+          <AppIcon className={cls.icon} SVG={EyeIcon} />
+          <AppText title={String(article?.views)} />
+        </div>
+
+        <div className={cls.articleInfo}>
+          <AppIcon className={cls.icon} SVG={CalendarIcon} />
+          <AppText title={article?.createdAt} />
+        </div>
+      </>
+    );
   }
 
   return (
--- src/shared/ui/AppText/AppText.tsx
+++ src/shared/ui/AppText/AppText.tsx
@@ -8,9 +8,14 @@ export enum AppTextTheme {
 }
 
 export enum AppTextAlign {
-  RIGHT = 'ight',
+  RIGHT = 'right',
   LEFT = 'left',
   CENTER = 'center',
+}
+
+export enum AppTextSize {
+  M = 'size_m',
+  L = 'size_l',
 }
 
 interface AppTextProps {
@@ -19,13 +24,21 @@ interface AppTextProps {
   text?: string;
   theme?: AppTextTheme;
   align?: AppTextAlign;
+  size?: AppTextSize;
 }
 
 const AppText = memo((props: AppTextProps) => {
-  const { className, title, text, theme = AppTextTheme.DEFAULT, align = AppTextAlign.LEFT } = props;
+  const {
+    className,
+    title,
+    text,
+    theme = AppTextTheme.DEFAULT,
+    align = AppTextAlign.LEFT,
+    size = AppTextSize.M,
+  } = props;
 
   return (
-    <div className={classNames(cls.AppText, [className, cls[theme], cls[align]])}>
+    <div className={classNames(cls.AppText, [className, cls[theme], cls[align], cls[size]])}>
       {title && <p className={cls.title}>{title}</p>}
       {text && <p className={cls.text}>{text}</p>}
     </div>
